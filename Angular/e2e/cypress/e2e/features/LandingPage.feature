48284fb3b2f388e39009c7558a15c6998dcb15cd
jonathanDicaprio <hectordicapriojonathan@gmail.com>
2023-06-15 14:37:29 +0200

    Group 4

--- Angular/e2e/cypress/e2e/features/LandingPage.feature
+++ Angular/e2e/cypress/e2e/features/LandingPage.feature
@@ -7,3 +7,22 @@ Feature: LandingPage Functionality
 
     Scenario: As a toh user, I want to navigate to landingpage in order to see the dashboard
         Then the user shall see the employee dashboard
+
+    Scenario:     As a toh user, I want to navigate to Heroes page
+        Given the user is on the landing page
+        Then clicks the heroes button
+        Then the user shall see the heroes screen
+
+    Scenario: As a TOH user, I want to navigate to dashboard in order to see top 4 Heroes displayed
+        Given the user is on the landing page
+        Then user wants to see the top 4 heroes displayed
+
+    Scenario: As a TOH user, I want to select a hero from the dashboard in order to see the heroe details displayed
+        Given the user is on the is on the heroes page
+        Then the user selects the first hero
+        Then the user will see the hero detail
+
+    Scenario: As a TOH user, I want to remove a hero in order to see the hero list decrease
+        Given the user is on the is on the heroes page
+        Then the user removes one hero
+        Then the count should be one less
--- Angular/e2e/cypress/e2e/step-definitions/LandingPage.spec.ts
+++ Angular/e2e/cypress/e2e/step-definitions/LandingPage.spec.ts
@@ -1,9 +1,59 @@
 import { Given, When, Then, And} from "cypress-cucumber-preprocessor/steps";
+import { TOF } from "../page-objects/TOF";
 
 Given(/^the user is on the landing page$/, () => {
 	cy.visit("http://localhost:4200/dashboard");
 });
 
 Then(/^the user shall see the employee dashboard$/, () => {
+	TOF.VerifyDash();
+});
+
+Given(/^the user is on the landing page$/, () => {
+	cy.visit("http://localhost:4200/dashboard");
+});
+
+Then(/^clicks the heroes button$/, () => {
+	TOF.HeroesButtonClick();
+});
+
+Then(/^the user shall see the heroes screen$/, () => {
+	TOF.VerifyHeroes();
+});
+
+
+Given(/^the user is on the landing page$/, () => {
+	cy.visit("http://localhost:4200/dashboard");
+});
+
+Then(/^user wants to see the top 4 heroes displayed$/, () => {
+	TOF.TopHeroCount();
+});
+
+
+
+Given(/^the user is on the is on the heroes page$/, () => {
+	cy.visit("http://localhost:4200/heroes");
+});
+
+Then(/^the user selects the first hero$/, () => {
+	TOF.SelectHero();
+});
+
+Then(/^the user will see the hero detail$/, () => {
+	TOF.VerifyHeroDetail();
+});
+
+
+Given(/^the user is on the is on the heroes page$/, () => {
+	cy.visit("http://localhost:4200/heroes");
+});
+
+Then(/^the user removes one hero$/, () => {
 	return true;
 });
+
+Then(/^the count should be one less$/, () => {
+	return true;
+});
+
